c10d9c80f0b21d4d4e05ed413a19069e2c3636bb
yycheng <yychenger@gmail.com>
2022-04-19 18:01:04 +0800

    feat: 加一个解法

--- src/twoSum.js
+++ src/twoSum.js
@@ -1,20 +1,34 @@
+/**
+ * map结构搞一下 一层循环 时间复杂度 O(n);
+ */
+// const twoSum = function (arr, target) {
+//   const map = new Map();
+//   for (var i = 0; i < arr.length; i++) {
+//     const remain = target - arr[i];
+//     if (map.has(remain)) {
+//       return [map.get(remain), i];
+//     } else {
+//       map.set(arr[i], i);
+//     }
+//     console.log(map, 'map');
+//   }
+// };
+
+// 暴力搞一下 两层循环  时间复杂度 O(n2);
 const twoSum = function (arr, target) {
-  const map = new Map();
   for (var i = 0; i < arr.length; i++) {
-    const remain = target - arr[i];
-    if (map.has(remain)) {
-      return [map.get(remain), i];
-    } else {
-      map.set(arr[i], i);
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [arr[i], arr[j]];
+      }
     }
-    console.log(map, 'map');
   }
 };
 
-const aa = new Map();
-aa.set(2, 0);
-aa.set(4, 1);
-aa.set(7, 2);
+// const aa = new Map();
+// aa.set(2, 0);
+// aa.set(4, 1);
+// aa.set(7, 2);
 
 // console.log(aa.get(4), 'has');
 
