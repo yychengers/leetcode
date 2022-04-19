9f32a77d5e2c97bb358c2304bb7937940fdd5c4d
yycheng <yychenger@gmail.com>
2022-04-19 18:34:56 +0800

    feat: 回文数字

--- src/palindrome-number.js
+++ src/palindrome-number.js
@@ -12,18 +12,33 @@ let url = 'https://leetcode-cn.com/problems/palindrome-number/';
  * @return {boolean}
  * 判断数组和反转数组的最终拼接成的str是否完全相等
  */
-var isPalindrome = function (x) {
-  const str = x.toString();
-  const arr = [];
-  for (var i = 0; i < str.length; i++) {
-    arr.push(str.charAt(i));
-  }
+// var isPalindrome = function (x) {
+//   const str = x.toString();
+//   const arr = [];
+//   for (var i = 0; i < str.length; i++) {
+//     arr.push(str.charAt(i));
+//   }
 
-  if (arr.join('') === arr.reverse().join('')) {
-    return true;
-  } else {
+//   if (arr.join('') === arr.reverse().join('')) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// 反转数组
+// 转成字符串，拆分成数组，反转，转成字符串 等等
+// var isPalindrome = function (x) {
+//   return x.toString() === x.toString().split('').reverse().join('');
+// };
+
+// 数组，数字，字符串转换
+var isPalindrome = function (x) {
+  if (x < 0) {
     return false;
   }
+  var y = parseInt(x.toString().split('').reverse().join(''));
+  return x === y ? true : false;
 };
 
-console.log(isPalindrome(12210));
+console.log(isPalindrome(13031));
