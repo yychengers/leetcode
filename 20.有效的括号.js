d0543bed50a71143552ce99cc3cc945c851246af
yycheng <yychenger@gmail.com>
2022-04-21 07:25:36 +0800

    feat: 对接leetcode官网题库

---
+++ 20.有效的括号.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  return s.length;
+};
+// @lc code=end
+
+console.log(isValid('a'));
