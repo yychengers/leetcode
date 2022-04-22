76c6cb27030efa4208cb2f7751143af7d447b4f4
yycheng <yychenger@gmail.com>
2022-04-22 10:22:37 +0800

    feat: 14 最长公共子前缀

---
+++ 14.最长公共前缀.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=javascript
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  return strs;
+};
+// @lc code=end
+
+let strs = ['flower', 'flow', 'flight']; // 输出 fl
+// let strs = ['dog', 'racecar', 'car']; // 输出 ''
+
+console.log(longestCommonPrefix(strs));
