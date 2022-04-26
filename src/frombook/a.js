ee1f341ef7f6ae2deb93eed220c9f978ad735019
yycheng <yychenger@gmail.com>
2022-04-27 07:32:48 +0800

    feat:新增排序算法文件 研究一下

--- src/frombook/a.js
+++ src/frombook/a.js
@@ -23,21 +23,48 @@
 //   }
 // };
 
-const arr = [1, 3, 5, 8];
-const sum = 8;
-const twoSumFast = (arr, sum) => {
-  let minIndex = 0;
-  maxIndex = arr.length - 1;
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[minIndex] + arr[maxIndex] > sum) {
-      maxIndex = maxIndex - 1;
-      continue;
+// const arr = [1, 3, 5, 8];
+// const sum = 8;
+// const twoSumFast = (arr, sum) => {
+//   let minIndex = 0;
+//   maxIndex = arr.length - 1;
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[minIndex] + arr[maxIndex] > sum) {
+//       maxIndex = maxIndex - 1;
+//       continue;
+//     }
+//     if (arr[minIndex] + arr[maxIndex] < sum) {
+//       minIndex = minIndex + 1;
+//       continue;
+//     }
+//     return [arr[minIndex], arr[maxIndex]];
+//   }
+// };
+// console.log(twoSumFast(arr, sum));
+
+function mySort(arr) {
+  const less = (v, w) => {
+    return v - w < 0;
+  };
+  const exch = (arr, i, j) => {
+    const t = arr[i];
+    arr[i] = arr[j];
+    arr[j] = t;
+  };
+  const show = (arr) => {
+    for (var i = 0; i < arr.length; i++) {
+      console.log(arr[i] + ' ');
+      console.log('\n');
     }
-    if (arr[minIndex] + arr[maxIndex] < sum) {
-      minIndex = minIndex + 1;
-      continue;
+  };
+  const isSorted = (arr) => {
+    for (var i = 1; i < arr.length; i++) {
+      if (less(arr[i], arr[i - 1])) {
+        return false;
+      }
     }
-    return [arr[minIndex], arr[maxIndex]];
-  }
-};
-console.log(twoSumFast(arr, sum));
+  };
+}
+
+const arr = [1, 4, 6, 3, 2, 8];
+mySort(arr);
