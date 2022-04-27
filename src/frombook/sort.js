36e9d00688222a6a6daf5ba845ddd9261649fe8b
yycheng <yychenger@gmail.com>
2022-04-28 07:18:20 +0800

    feat: 插入排序

--- src/frombook/sort.js
+++ src/frombook/sort.js
@@ -22,20 +22,41 @@ import { swap } from './util/index.js';
 //   return arr;
 // }
 
-function selectSort(arr) {
-  // 找出最小的元素
-  // 将最小的元素和数组第一个位置的元素交换
+// ----------------------------------
 
-  for (var i = 0; i < arr.length; i++) {
-    var minIndex = i;
-    for (var j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
+// function selectSort(arr) {
+//   // 找出最小的元素
+//   // 将最小的元素和数组第一个位置的元素交换
+
+//   for (var i = 0; i < arr.length; i++) {
+//     var minIndex = i;
+//     for (var j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+//     swap(arr, i, minIndex);
+//   }
+//   return arr;
+// }
+// const arr = [4, 3, 1];
+// console.log(selectSort(arr));
+
+// ----------------------------------
+
+// 插入排序
+// 从索引开始，将每一个元素与他前边的元素进行比较，如果小于之前的元素，就交换位置
+function insertSort(arr) {
+  const n = arr.length;
+  for (var i = 0; i < n; i++) {
+    for (var j = i; j < n; j++) {
+      if (arr[j] < arr[i]) {
+        swap(arr, i, j);
       }
     }
-    swap(arr, i, minIndex);
   }
   return arr;
 }
-const arr = [4, 3, 1];
-console.log(selectSort(arr));
+
+const arr = [1, 4, 7, 20, 8];
+console.log(insertSort(arr));
